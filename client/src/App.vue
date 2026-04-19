668219ad89494d895eb2acabf2458ea9770aee25
jopatk <71216198@qq.com>
2026-04-20 07:45:58 +0800

    修复一些问题

--- client/src/App.vue
+++ client/src/App.vue
@@ -45,10 +45,13 @@
   });
 
   const formattedTime = computed(() =>
-    now.value.toLocaleTimeString([], {
+    now.value.toLocaleString('en-US', {
+      month: 'long',
+      day: 'numeric',
       hour: '2-digit',
       minute: '2-digit',
       second: '2-digit',
+      hour12: false,
     })
   );
 
